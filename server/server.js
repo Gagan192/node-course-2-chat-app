c07a858c97d24686646371ae86b2c554c7548c2e
Gagandeep <gagangoyal192@gmail.com>
2018-08-18 17:26:05 +0530

    create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require("http");
 const express = require('express');
 const socketIO =require('socket.io');
 
+const {generateMessage} =require('./utils/message');
 const publicPath=path.join(__dirname,'../public');
 const port=process.env.PORT || 3000;
 var app = express();
@@ -16,27 +17,15 @@ io.on('connection',(socket)=>{
   console.log('New User Connected');
 
   //socket.emit from Admin text Welcome to the chat app
-  socket.emit('newMessage',{
-    from:'Admin',
-    text: 'Welcome to the chat app',
-    createdAt: new Date().getTime()
-  });
+  socket.emit('newMessage', generateMessage('Admin','Welcome to the chat app'));
 
   //socket.broadcast.emit from Admin Text NEW user Joined
-    socket.broadcast.emit('newMessage',{
-      from:"Admin",
-      text: 'New User Joined',
-      createdAt: new Date().getTime()
-    });
+    socket.broadcast.emit('newMessage',generateMessage('Admin','New User Joined'));
 
   socket.on('createMessage',(message)=>{
     console.log('Created Message',message);
     //Emitting to All That are Connected
-    io.emit('newMessage',{
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
+    io.emit('newMessage',generateMessage('message.from','message.text'));
 
 // Emiting TO all But Not Himself
     // socket.broadcast.emit('newMessage',{
