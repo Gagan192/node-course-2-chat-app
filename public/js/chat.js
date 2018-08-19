69ff0cf15c2d9a87225dde0241af993e4dd33672
Gagandeep <gagangoyal192@gmail.com>
2018-08-19 15:32:59 +0530

    add users class and update user list

--- public/js/chat.js
+++ public/js/chat.js
@@ -18,7 +18,15 @@ function scrollToBottom(){
 }
 
   socket.on('connect',function(){
-    console.log('Connected to Server');
+    var params = jQuery.deparam(window.location.search);
+    socket.emit('join',params,function(err){
+      if(err){
+        alert(err);
+        window.location.href='/';
+      }else{
+        console.log("No error");
+      }
+    });
   });
 
 
@@ -42,6 +50,15 @@ function scrollToBottom(){
   socket.on('disconnect',function(){
     console.log('Disconnected from the server');
   });
+
+socket.on('updateUserList',function(users){
+  var ol = jQuery('<ol></ol>');
+
+  users.forEach(function(user){
+    ol.append(jQuery('<li></li>').text(user));
+  });
+  jQuery('#users').html(ol);
+});
 
   // socket.emit('createMessage',{
   //   from: 'frank',
